3b33d19a635acbbd47ffcd1cd95d6eaa2c706c27
ninandi78 <140152594+ninandi78@users.noreply.github.com>
2023-10-03 23:12:26 -0600

    Add files via upload

---
+++ ESTRELLA_ACTIVIDAD10/index.ts
@@ -0,0 +1,12 @@
+const express = require("express")
+const app=express();
+
+const example = function(){
+    console.log("Estrella Ninandi 10 A || Desarrollo móvil integral")
+
+
+}
+app.listen(
+    9000,
+    example
+)
